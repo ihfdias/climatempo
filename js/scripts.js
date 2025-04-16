d23c5baf92ab01e1196af9a3863ee9980a5e9585
Igor Dias <ihfdias.dev@gmail.com>
2025-04-16 16:04:07 -0300

    enter adicionado

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,11 @@ searchBtn.addEventListener("click", (e) => {
     showWeatherData(city);
 
 })
+
+cityInput.addEventListener("keyup", (e) => {
+    if(e.code === "Enter") {
+        const city = e.target.value
+
+        showWeatherData(city);
+    }
+})
