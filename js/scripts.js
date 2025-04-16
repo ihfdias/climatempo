6e41efbab9a2ae7c97245d5899ecc2b441cb9625
Igor Dias <ihfdias.dev@gmail.com>
2025-04-16 15:58:48 -0300

    Clima tempo projeto

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+// Variáveis e seleção de elementos 
+const apiKey = "d359af8fbafdc11a70c8df11f5887bf0";
+const apiCountryURL = "https://flagsapi.com/:country_code/:style/:size.png";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+const weatherContainer = document.querySelector("#weather-data")
+
+// Funções 
+const getWeatherData = async(city) => {
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+
+    return data;
+
+}
+
+const showWeatherData = async (city) => {
+   const data = await getWeatherData(city);
+
+   cityElement.innerText = data.name;
+   tempElement.innerText = parseInt(data.main.temp);
+   descElement.innerText = data.weather[0].description;
+   weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+   countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+   humidityElement.innerText = `${data.main.humidity}%`;
+   windElement.innerText = `${data.wind.speed}km/h`;
+
+   weatherContainer.classList.remove("hide")
+    
+
+}
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+
+// Eventos
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+     const city = cityInput.value;  
+
+    showWeatherData(city);
+
+})
